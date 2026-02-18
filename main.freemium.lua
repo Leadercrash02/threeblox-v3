a8090555e45cca82a78432cd523465510c209409
Leadercrash02 <101663641+Leadercrash02@users.noreply.github.com>
2026-02-18 08:16:01 +0700

    Update main.freemium.lua

--- main.freemium.lua
+++ main.freemium.lua
@@ -641,6 +641,7 @@ if InfoPage then
 
     AddChangeLine("(+) Added New GUI layout")
     AddChangeLine("(+) Added New GUI layout")
+    AddChangeLine("(+) Added New GUI layout")
 end
 
 --==================================================
